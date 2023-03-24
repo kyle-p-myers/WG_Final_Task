f7a1e851c8b78cd4f8dfe181f76631e783a83a70
Kyle Myers <kmyers2438@gmail.com>
2023-03-24 11:32:45 -0600

    Initial Commit

---
+++ WG_Final_Task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WG_Final_Task
+//
+//  Created by Kyle on 3/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
